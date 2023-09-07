8d7df977929518e89601fbd1d784c4637272df1a
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-07 17:24:30 -0400

    Test the fizzbuzz method when the input number is 10

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -25,5 +25,9 @@ describe Solver do
     it 'returns "fizz" when the number is divisible by 3' do
       expect(Solver.fizzbuzz(9)).to eq("fizz")
     end
+
+    it 'returns "buzz" when the number is divisible by 5' do
+      expect(Solver.fizzbuzz(10)).to eq("buzz")
+    end
   end
 end
