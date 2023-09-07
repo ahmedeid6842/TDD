2e395633479f753e6f1e128eafc3876d14cd8da8
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-08 00:18:12 +0300

    Added test for factorial method

---
+++ spec/solver_spec.rb
@@ -0,0 +1,17 @@
+require_relative "../solver"
+
+describe Solver do
+  describe "#factorial" do
+    it "returns 1 for factorial of 0" do
+      expect(Solver.factorial(0)).to eq(1)
+    end
+
+    it "returns the factorial of a positive number" do
+      expect(Solver.factorial(5)).to eq(120)
+    end
+
+    it "raises an exception for a negative number" do
+      expect { Solver.factorial(-1) }.to raise_error(ArgumentError)
+    end
+  end
+end
